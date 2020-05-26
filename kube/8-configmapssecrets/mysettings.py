635fa7657200ba9e1816889c7631dc0ebd82be33
Lim Swee Tat <sweetat+github@xfers.com>
2020-05-27 00:23:02 +0800

    Adding deployment files to demo configmap and secrets

---
+++ kube/8-configmapssecrets/mysettings.py
@@ -0,0 +1,12 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': 'mysql',
+        'PORT': '3306',
+        'USER': 'mydjango',
+        'PASSWORD': 'password',
+        'NAME': 'mydjango',
+    }
+}
+
+
